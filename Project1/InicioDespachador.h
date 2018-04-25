5a5c15b594d43cc6e7a0c58e6782f3f7a67d0f48
unknown <9395310@labprog.local>
2018-04-25 11:50:13 -0300

    Mudança nome botoes

--- Project1/InicioDespachador.h
+++ Project1/InicioDespachador.h
@@ -34,6 +34,10 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,11 +52,50 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"InicioDespachador";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(56, 77);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(149, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Obras Marcadas";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(56, 125);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(149, 26);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Obras Pendentes";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(167, 216);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// InicioDespachador
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"InicioDespachador";
+			this->Text = L"InicioDespachador";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
